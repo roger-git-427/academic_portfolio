9b054a1aa58c0761b9c496d14d7b0ae9c9959be2
Avril <avril03michruiz06@gmail.com>
2025-06-02 16:02:30 -0600

    feat: added preprocessing

---
+++ my-kedro-project/final-tca-pipeline-reto/src/final_tca_pipeline_reto/pipelines/reservations_data_preprocessing/reservations_preprocessing/pipeline.py
@@ -0,0 +1,57 @@
+"""
+This is a boilerplate pipeline 'reservations_preprocessing'
+generated using Kedro 0.19.12
+"""
+
+from kedro.pipeline import node, Pipeline, pipeline  # noqa
+
+from .nodes import (
+    select_columns,
+    merge_lookup_tables,
+    convert_dates,
+    enforce_types_and_basic_filters,
+    normalise_city,
+    remove_outliers_percentile,
+    replace_h_num_persons,
+    filtered_df,
+    build_daily_occupancy,
+    prepare_features
+)
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([
+        node(select_columns, "raw_reservaciones", "reservations_base", name="select_cols"),
+        node(merge_lookup_tables, dict(
+            reservations="reservations_base",
+            canales="raw_canales",
+            empresas="raw_empresas",
+            agencias="raw_agencias",
+            estatus="raw_estatus_reservaciones"
+        ), "reservations_merged", name="merge_lookups"),
+        node(convert_dates, "reservations_merged", "reservations_dates", name="convert_dates"),
+        node(enforce_types_and_basic_filters, "reservations_dates", "reservations_typed", name="types_filters"),
+        node(normalise_city, "reservations_typed", "reservations_city_norm", name="normalise_city"),
+        node(remove_outliers_percentile, [
+            "reservations_city_norm",
+            "params:outlier_exclude_cols",
+            "params:outlier_pct"
+        ], "reservations_iqr", name="outliers_pct"),
+        node(replace_h_num_persons, "reservations_iqr", "reservations_grouped", name="replace_persons"),
+        node(filtered_df, "reservations_grouped", "reservations_filtered", name="filter_reservations"),
+        node(
+            build_daily_occupancy,
+            inputs=[
+                "reservations_filtered", 
+                "params:date_filter.START_DATE", 
+                "params:date_filter.END_DATE"
+            ],
+            outputs="rooms_by_date",
+            name="build_occupancy"
+        ),
+        node(
+            func=prepare_features,
+            inputs="rooms_by_date",
+            outputs="df_features",
+            name="prepare_features"
+        ),
+    ])
